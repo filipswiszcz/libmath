35e51b33e636b0d58d087ab1a4248157dffc877c
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2026-04-27 12:26:01 +0200

    init jet math

---
+++ jmath.h
@@ -0,0 +1,14 @@
+#ifndef __LIBMATH_H__
+#define __LIBMATH_H__
+
+#define PI 3.14159265358979323846
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif // !__LIBMATH_H__
